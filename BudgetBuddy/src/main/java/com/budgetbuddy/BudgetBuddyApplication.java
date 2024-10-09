603a93869a4b5065e638e8dc389a394833457672
Alex <alexmb2354@gmail.com>
2024-10-09 18:08:31 -0400

    Test Commit
    I made a comment :smile:

--- BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BudgetBuddyApplication {
-
+/*This is a comment probably*/
     public static void main(String[] args) {
         SpringApplication.run(BudgetBuddyApplication.class, args);
     }
