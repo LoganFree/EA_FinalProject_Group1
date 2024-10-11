a2735b75695a46b98b5ffb541aaa208a89621dbf
Anthony Johnson <johns9au@mail.uc.edu>
2024-10-11 17:17:19 -0400

    interface
    Interface changes to dao

--- BudgetBuddy/src/main/java/com/budgetbuddy/dao/IEarningDAO.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/dao/IEarningDAO.java
@@ -1,4 +1,14 @@
 package com.budgetbuddy.dao;
+import com.budgetbuddy.dto.Earning;
+
+import java.util.List;
 
 public interface IEarningDAO {
-}
+        Earning save(Earning earning) throws Exception;
+
+        List<Earning> fetchAll();
+
+       Earning fetch(int id);
+
+        void delete(int id);
+    }
--- BudgetBuddy/src/main/java/com/budgetbuddy/dao/IExpenseDAO.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/dao/IExpenseDAO.java
@@ -1,4 +1,16 @@
 package com.budgetbuddy.dao;
 
+import com.budgetbuddy.dto.Expense;
+
+import java.util.List;
+
 public interface IExpenseDAO {
+        Expense save(Expense expense) throws Exception;
+
+        List<Expense> fetchAll();
+
+        Expense fetch(int id);
+
+        void delete(int id);
+
 }
