78e34894d1ff23819711d2d4d2577610e77be5a5
LoganFree <loganfree18@gmail.com>
2024-10-11 16:39:40 -0400

    Fix

--- BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
@@ -2,7 +2,7 @@ package com.budgetbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*This is the UI Branch*/
+
 @SpringBootApplication
 public class BudgetBuddyApplication {
     public static void main(String[] args) {
