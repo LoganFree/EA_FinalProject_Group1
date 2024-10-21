5407dd938533af3d08fba3dd5df090a9c77a0546
LoganFree <loganfree18@gmail.com>
2024-10-20 22:34:02 -0400

    Added test calculation service

---
+++ BudgetBuddy/src/main/java/com/budgetbuddy/service/CalculationService.java
@@ -0,0 +1,40 @@
+package com.budgetbuddy.service;
+
+public class CalculationService {
+
+    private boolean earnIsSalary;
+    private double earnAmount;
+    private double weeklyHours;
+
+    public double weeklyBudget;
+
+
+    public CalculationService(boolean earnIsSalary, double earnAmount, double weeklyHours) {
+
+        //get base weekly budget
+        if (earnIsSalary == true){
+            //divide salary wage by how many weeks there are in a year
+            //this yields the weekly budget
+            weeklyBudget = earnAmount/52;
+        }
+        else if (earnIsSalary == false){
+            //multiply weekly wage by how many hours are worked in a week
+            //this yields the weekly budget
+            weeklyBudget = earnAmount*weeklyHours;
+        }
+
+
+        //subtract weekly expenses
+
+
+
+
+        //subtract monthly bill if it is due in the week
+        //get from WeekDayService?
+
+
+
+    }
+
+
+}
