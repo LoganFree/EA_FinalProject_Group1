67a3f186d517898c023fe230071515686985e5f3
LoganFree <loganfree18@gmail.com>
2024-10-20 22:01:54 -0400

    Added as week selector and week service

--- BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyController.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyController.java
@@ -2,6 +2,7 @@ package com.budgetbuddy;
 
 import com.budgetbuddy.dto.Bill;
 import com.budgetbuddy.service.BillService;
+import com.budgetbuddy.service.WeekService;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -12,53 +13,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-
 @Controller
 public class BudgetBuddyController {
+
     @Autowired
     BillService billService;
 
+    @Autowired
+    private WeekService weekService;
+
     @RequestMapping("/")
-    public String index(Model model)
-    {
-
-
+    public String index(Model model) {
         model.addAttribute("page", "home");
         return "startpage";
     }
 
     @RequestMapping("/entry-form")
-    public String entryForm(Model model)
-    {
+    public String entryForm(Model model) {
         Bill bill = new Bill();
         bill.setBillAmount(100.00);
         //bill.setBillDueDate(new Date("10-10-2024"));
         bill.setBillDescription("Test");
-
         model.addAttribute("page", "entry");
         model.addAttribute(bill);
         return "entryform";
     }
 
     @RequestMapping("/dashboard")
-    public String dashboard(Model model)
-    {
+    public String dashboard(Model model) {
         model.addAttribute("page", "dashboard");
         return "dashboard";
     }
 
-
     @RequestMapping("/fragments/styles.css")
-    public String styles(Model model)
-    {
+    public String styles(Model model) {
         return "fragments/styles.css";
     }
 
     @RequestMapping("/saveBill")
-    public String saveBill(Bill bill)
-    {
+    public String saveBill(Bill bill) {
         try {
             billService.save(bill);
         } catch (Exception e) {
@@ -69,5 +62,11 @@ public class BudgetBuddyController {
         return "entryform";
     }
 
-
+    @PostMapping("/week-selected")
+    public String getWeekDates(@RequestParam("week") String week, Model model) {
+        String formattedWeek = weekService.getWeekDates(week);
+        model.addAttribute("weekDays", formattedWeek);
+        model.addAttribute("selectedWeek", week);
+        return "dashboard"; // Ensure "dashboard.html" is in src/main/resources/templates
+    }
 }
