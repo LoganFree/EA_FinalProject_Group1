6cd9a5331cafecb3289b84785e8c1f6b85cde71b
LoganFree <loganfree18@gmail.com>
2024-10-20 22:10:17 -0400

    Added Comments

--- BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyController.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyController.java
@@ -15,19 +15,25 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BudgetBuddyController {
-
     @Autowired
     BillService billService;
-
     @Autowired
     private WeekService weekService;
 
+    //call for CSS
+    @RequestMapping("/fragments/styles.css")
+    public String styles(Model model) {
+        return "fragments/styles.css";
+    }
+
+    //START PAGE
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("page", "home");
         return "startpage";
     }
 
+    //ENTRY FORM
     @RequestMapping("/entry-form")
     public String entryForm(Model model) {
         Bill bill = new Bill();
@@ -39,19 +45,11 @@ public class BudgetBuddyController {
         return "entryform";
     }
 
-    @RequestMapping("/dashboard")
-    public String dashboard(Model model) {
-        model.addAttribute("page", "dashboard");
-        return "dashboard";
-    }
-
-    @RequestMapping("/fragments/styles.css")
-    public String styles(Model model) {
-        return "fragments/styles.css";
-    }
-
+    //called when a bill is added on the entry form
     @RequestMapping("/saveBill")
-    public String saveBill(Bill bill) {
+    public String saveBill(Bill bill, Model model) {
+        model.addAttribute("page", "entry");
+
         try {
             billService.save(bill);
         } catch (Exception e) {
@@ -62,13 +60,24 @@ public class BudgetBuddyController {
         return "entryform";
     }
 
+    //DASHBOARD
+    @RequestMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAttribute("page", "dashboard");
+        return "dashboard";
+    }
+
+    //called when a week is selected in the dashboard
     @PostMapping("/week-selected")
     public String getWeekDates(@RequestParam("week") String week, Model model) {
+        //set page to active
         model.addAttribute("page", "dashboard");
 
+        //call weekService
         String formattedWeek = weekService.getWeekDates(week);
         model.addAttribute("weekDays", formattedWeek);
         model.addAttribute("selectedWeek", week);
         return "dashboard";
     }
+
 }
