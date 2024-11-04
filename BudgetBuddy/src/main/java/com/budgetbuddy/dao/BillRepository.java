24e8f7748ac972e49aea97ed1d23afda624a31f5
Loc Nguyen <loc.nguyen.leo@gmail.com>
2024-11-04 12:12:08 -0500

    Ready to merge for code review

--- BudgetBuddy/src/main/java/com/budgetbuddy/dao/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.budgetbuddy.dao;
-
-import com.budgetbuddy.dto.Bill;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BillRepository extends CrudRepository<Bill, Integer> {
-}
--- BudgetBuddy/src/main/java/com/budgetbuddy/dao/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.budgetbuddy.dao;
-
-import com.budgetbuddy.dto.Expense;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
-}
