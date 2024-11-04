7ea39d645187907d497190dd0f4dd8a1e1b9061e
LoganFree <loganfree18@gmail.com>
2024-11-04 12:54:28 -0500

    Fixed Retrofit

--- BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/BudgetBuddyApplication.java
@@ -2,10 +2,12 @@ package com.budgetbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BudgetBuddyApplication {
     public static void main(String[] args) {
         SpringApplication.run(BudgetBuddyApplication.class, args);
     }
 }
+
