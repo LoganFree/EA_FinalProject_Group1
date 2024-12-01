9fb438b17f7435bf9dac33d86d0056730bb24eaa
LoganFree <148898734+LoganFree@users.noreply.github.com>
2024-12-01 16:04:57 -0500

    Minor Changes

--- BudgetBuddy/src/main/java/com/budgetbuddy/service/ExpenseService/ExpenseService.java
+++ BudgetBuddy/src/main/java/com/budgetbuddy/service/ExpenseService/ExpenseService.java
@@ -5,6 +5,8 @@ import com.budgetbuddy.dao.ExpenseDAO.IExpenseDAO;
 import com.budgetbuddy.dto.Category;
 import com.budgetbuddy.dto.Expense;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +31,14 @@ public class ExpenseService implements IExpenseService {
     public List<Category> getCategories() throws Exception{
         return categoryDAO.getCategories();
     }
+    
+    //@Cacheable("expenses")
+    @Override
+    public List<Expense> getAllExpenses() {
+        return expenseDAO.getAllExpenses();
+    }
 
+    //@CachePut(value = "expenses", key = "#expense")
     @Override
     public Expense save(Expense expense) {
         return expenseDAO.save(expense);
@@ -40,15 +49,10 @@ public class ExpenseService implements IExpenseService {
         return null;
     }
 
-
+    //@CacheEvict(value = "expenses", key = "#id")
     @Override
     public void deleteExpense(int id) {
         expenseDAO.deleteExpense(id);
-    }
-
-    @Override
-    public List<Expense> getAllExpenses() {
-        return expenseDAO.getAllExpenses();
     }
 
     @Override
